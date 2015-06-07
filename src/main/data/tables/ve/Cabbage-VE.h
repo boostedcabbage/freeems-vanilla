0074993be26fc5b37b7e1cd3c20b4b8a86c35c2d
Chris Beach <chris19981@gmail.com>
2015-06-06 20:30:19 -0400

    Changed table filenames to avoid renaming in the future

---
+++ src/main/data/tables/ve/Cabbage-VE.h
@@ -0,0 +1,53 @@
+/* FreeEMS - the open source engine management system
+ *
+ * Copyright 2012 Fred Cooke
+ *
+ * This file is part of the FreeEMS project.
+ *
+ * FreeEMS software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FreeEMS software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with any FreeEMS software.  If not, see http://www.gnu.org/licenses/
+ *
+ * We ask that if you make any changes to this file you email them upstream to
+ * us at admin(at)diyefi(dot)org or, even better, fork the code on github.com!
+ *
+ * Thank you for choosing FreeEMS to run your engine!
+ */
+
+
+/** @file
+ *
+ * @ingroup dataInitialisers
+ *
+ * @brief boostedcabbage's Neon's VE. Flat 40 for now.
+ */
+
+//200      700      1200      1600      2000      2400      2800      3200      3600      4000      4400      4800      5200      5600      6000      6400      6800      7200      7600
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 15 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 20 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 25 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 30 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 35 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 40 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 45 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 50 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 55 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 60 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 65 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 70 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 75 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 80 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 85 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 90 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 95 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   // 100 kPa
+VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40),   VE(40)   // 105 kPa
